7f4c295852168ab8a00a3f229b21371248abf96e
Oleg Korsunsky <wd@dizaina.net>
2017-07-16 02:04:31 +0300

    coop!

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var carSocket = null;
-var browserWebSocket = null;
+const Cars = {};
+const CarsByPlayerId = {};
+let carIdCounter = 0;
 
 
 // Car socket server
-var net = require('net');
-var server = new net.Server;
+const net = require('net');
+const server = new net.Server;
 
 server.listen({
     port: 8000
@@ -12,46 +13,66 @@ server.listen({
     server.on('connection', function connected(socket) {
         console.log('Car connected!');
 
-        carSocket = socket;
+        const carId = carIdCounter;
+        carIdCounter++;
+
+        const car = Cars[carId] = {
+            socket
+        };
 
         socket.on('close', function closed(hadError) {
-            console.log('Connection to the car was closed, hadError:', hadError);
-            carSocket = null;
+            console.log(`<${carId}> Connection to the car was closed, hadError:`, hadError);
+            delete Cars[carId];
+            if (car.playerId) delete CarsByPlayerId[car.playerId];
         });
 
         socket.on('data', function receivedData(data) {
-            var dataString = data.toString();
+            const dataString = data.toString();
 
-            console.log('The car says:', dataString);
-
-            if (browserWebSocket) browserWebSocket.send(dataString);
+            console.log(`<${carId}> The car says: ${dataString}>`);
         });
 
         socket.on('error', function(err) {
-            console.log('Socket error:', err.stack);
+            console.log(`<${carId}> Socket error:`, err.stack);
         });
     });
 });
 
 
 // Browser websocket server
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({port: 80});
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({port: 80});
 
 wss.on('connection', function connection(ws) {
     console.log('Browser connected!');
 
-    browserWebSocket = ws;
-
     ws.on('message', function incoming(message) {
         console.log('Browser says:', message);
 
-        if (carSocket) carSocket.write(message);
+        const parsedMsg = /<(\d+)>(.*)/.exec(message);
+
+        if (!parsedMsg || !parsedMsg[1] || !parsedMsg[2]) return;
+
+        const playerId = parsedMsg[1];
+        let playersCar = CarsByPlayerId[playerId];
+        
+        if (!playersCar) {
+            for (let key in Cars) {
+                const car = Cars[key];
+
+                if (!car.playerId) {
+                    car.playerId = playerId;
+                    playersCar = CarsByPlayerId[playerId] = car;
+                    break;
+                }
+            }
+        }
+
+        if (playersCar) playersCar.socket.write(parsedMsg[2]);
     });
 
     ws.on('close', function(code) {
         console.log('Connection to the browser was closed, code:', code);
-        browserWebSocket = null;
     });
 
     ws.on('error', function(err) {
